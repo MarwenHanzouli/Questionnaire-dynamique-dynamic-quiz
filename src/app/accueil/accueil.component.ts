e10a64b4a36b7b823803c3b2aacf14e05b09edcf
Marwen Hanzouli <marwenhanzouli@gmail.com>
2019-12-16 02:59:30 +0100

    accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,54 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
-
-  questForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.initForm();
-  }
-  initForm(){
-    this.questForm=this.formBuilder.group({
-      titre:['', Validators.required],
-      simples: this.formBuilder.array([]),
-      QCM: this.formBuilder.array([])
-    });
-  }
+    
+    }
 
-  onSubmitForm(){
-    const formValue=this.questForm.value;
-    console.log(formValue['titre']);
-    console.log(formValue['simples']);
-    console.log(formValue['QCM']);
-  }
-
-  getQuestionsSimples(): FormArray {
-    return this.questForm.get('simples') as FormArray;
-  }
-  onAjouterQuestionSimple()
-  {
-    const control=this.formBuilder.control(null,Validators.required);
-    this.getQuestionsSimples().push(control); 
-  }
-  onSupprimerQuestionSimple(index){
-    this.getQuestionsSimples().removeAt(index);
-  }
-
-  getQCM(): FormArray {
-    return this.questForm.get('QCM') as FormArray;
-  }
-  onAjouterQCM()
-  {
-    const control=this.formBuilder.control(null,Validators.required);
-    this.getQCM().push(control);
-  }
-  onSupprimerQCM(index){
-    this.getQCM().removeAt(index);
-  }
+    
 }

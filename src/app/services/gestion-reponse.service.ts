f339492704ae7cc9dc0ef20200fc616d78ec7657
hanzouliarfaoui <hanzouliarfaouli@gmail.com>
2019-12-18 00:59:00 +0100

    add service reponse

---
+++ src/app/services/gestion-reponse.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Reponse } from '../models/Reponse.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GestionReponseService {
+
+  constructor() { }
+
+  ajouterReponse(reponse: Reponse){
+    
+  }
+}

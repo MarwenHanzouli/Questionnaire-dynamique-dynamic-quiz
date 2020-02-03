93cc5311c3ea88ff3e1d614b1549b9a1ae1dcb73
hanzouliarfaoui <hanzouliarfaouli@gmail.com>
2020-02-03 19:54:23 +0100

    change colorPie

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -3,6 +3,7 @@ import { ChartsModule } from 'ng2-charts';
 import { ChartOptions } from 'chart.js';
 import { StatistiquesService } from '../services/statistiques.service';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-statistiques',
@@ -21,9 +22,18 @@ export class StatistiquesComponent implements OnInit {
   nbrRepsParEmail: number[]=[];
   private reponsesEmailSubscription: Subscription;
 
-  constructor(private statService: StatistiquesService) { }
-
-  public colors = [
+  constructor(private statService: StatistiquesService,
+              private route: ActivatedRoute) { }
+  allColors= ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)','rgba(186,175,34,0.3)',
+           'rgba(242,19,19,0.3)', 'rgba(135, 4, 4,0.3)', 'rgba(173, 221, 183,0.3)','rgba(30, 137, 191,0.3)',
+           'rgba(25, 26, 33,0.3)', 'rgba(124, 88, 102,0.3)', 'rgba(193, 152, 38,0.3)','rgba(160, 42, 183,0.3)',
+           'rgba(86, 0, 33,0.3)', 'rgba(89, 122, 99,0.3)', 'rgba(86, 89, 65,0.3)','rgba(98, 112, 0,0.3)']
+  colorsPie = [
+    {
+      backgroundColor: []
+    }
+  ];
+  colorsDoughnut = [
     {
       backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)','rgba(186,175,34,0.3)'],
     },
@@ -54,9 +64,9 @@ export class StatistiquesComponent implements OnInit {
   ngOnInit() {
     this.initDataForPipe();
     this.initDataForDoughnut();
-    console.log(this.emails);
   }
   initDataForPipe(){
+    let backGroundPie=[];
     this.statService.getReponsesParQuestionnaire();
     this.reponsesSubscription=this.statService.nbrRepParQuestSubject.subscribe(
       (rep: any[]) => {
@@ -64,23 +74,47 @@ export class StatistiquesComponent implements OnInit {
       }
     );
     this.statService.emitNbrRepParQuestSubject();
+    let i=0;
     this.nbrRepParQuest.forEach((element)=> {
       this.titreQuestions.push(element['titre']);
       this.nbrRep.push(element['nbrRep']);
+      backGroundPie.push(this.allColors[i]);
+      i++;
     });
+    this.colorsPie[0].backgroundColor=backGroundPie;
   }
+
   initDataForDoughnut(){
+    /*this.route.data.subscribe((data) => {
+      console.log(data['message']);
+      let backGroundDoughnut=[];
+      let j=0;
+      this.nbrRepParEmail.forEach((element) => {
+        this.emails.push(element['email']);
+        this.nbrRepsParEmail.push(element['nbrRep']);
+        backGroundDoughnut.push(this.allColors[j]);
+        j++;
+      });
+      this.colorsDoughnut[0].backgroundColor=backGroundDoughnut;
+    } );*/
+    //console.log(this.route.data['_value'].message);
     this.statService.getReponsesParEmail();
     this.reponsesEmailSubscription=this.statService.nbrRepParEmailSubject.subscribe(
       (rep: any[]) => {
         this.nbrRepParEmail=rep;
+        console.log(this.nbrRepParEmail);
       }
     );
     this.statService.emitNbrRepParEmailSubject();
-    this.nbrRepParEmail.forEach((element) => {
-      this.emails.push(element['email']);
-      this.nbrRepsParEmail.push(element['nbrRep']);
-    });
+    let backGroundDoughnut=[];
+      let j=0;
+      this.nbrRepParEmail.forEach((element) => {
+        this.emails.push(element['email']);
+        this.nbrRepsParEmail.push(element['nbrRep']);
+        backGroundDoughnut.push(this.allColors[j]);
+        j++;
+      });
+      this.colorsDoughnut[0].backgroundColor=backGroundDoughnut;
   }
   ngOnDestroy(){
     this.reponsesSubscription.unsubscribe();
